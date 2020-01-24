3355ca446174c31ac03b1e93307d54a9a785b608
Mitar Perovic <perovicmitar@gmail.com>
2020-01-24 11:00:21 +0100

    Initial commit

---
+++ src/beans/User.java
@@ -0,0 +1,8 @@
+package beans;
+
+public class User {
+    private String username;
+    private String password;
+
+
+}
---
+++ src/rest/SparkMainApp.java
@@ -0,0 +1,36 @@
+package rest;
+
+import beans.User;
+import com.google.gson.Gson;
+import spark.Session;
+
+import java.io.File;
+import java.io.IOException;
+
+import static spark.Spark.*;
+
+public class SparkMainApp {
+
+    private static Gson gson = new Gson();
+
+    public static void main(String[] args) throws IOException {
+        port(8080);
+        staticFiles.externalLocation(new File("./static").getCanonicalPath());
+
+        get("/",(req,res)->{
+            return "Fcking wrksss";
+        });
+
+        // Cisto da se ima
+        get("/rest/demo/logout", (req, res) -> {
+            res.type("application/json");
+            Session ss = req.session(true);
+            User user = ss.attribute("user");
+
+            if (user != null) {
+                ss.invalidate();
+            }
+            return true;
+        });
+    }
+}
